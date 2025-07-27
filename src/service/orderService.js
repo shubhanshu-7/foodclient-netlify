f101fb9cb968e1034ca768f47f931f95a65fd2ab
shubhanshu-7 <shubhanshusahu71@gmail.com>
2025-07-27 20:07:06 +0530

    updated url

--- src/service/orderService.js
+++ src/service/orderService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/orders";
+const API_URL = "https://foodrestapi-production-7cdd.up.railway.app/api/orders";
 
 export const fetchUserOrders = async (token) => {
     try {
